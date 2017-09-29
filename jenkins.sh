a156c43206f31887259ca25259edb9f70f832105
Martijn Koster <mak-github@greenhills.co.uk>
2017-09-29 12:07:14 +0100

    add missing files

---
+++ jenkins.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+git log --oneline --no-abbrev -n 1
+
+NOW=$(date '+%Y%m%d%H%M%S')
+echo "Building with timestamp $NOW"
+
+if [[ ! -z "${HUB_USERNAME:-}" && ! -z "${HUB_PASSWORD:-}" ]]; then
+  docker login --username "$HUB_USERNAME" --password "$HUB_PASSWORD"
+  DO_PUSH=1
+fi
+
+CREATED=()
+
+function build_and_push {
+  local d=$1
+  local IMAGE_NAME="lucidworks/fusion-builder-$d:$NOW"
+  cat >"$d/build.info" <<EOM
+IMAGE=$IMAGE_NAME
+GIT_COMMIT=${GIT_COMMIT:-}
+EOM
+  (cd "$d"; docker build -t "$IMAGE_NAME" .)
+  docker tag "$IMAGE_NAME" "lucidworks/fusion-builder-$d:latest"
+  CREATED+=("$IMAGE_NAME")
+  if [[ ! -z "${DO_PUSH:-}" ]]; then
+    docker push "$IMAGE_NAME"
+    docker push "lucidworks/fusion-builder-$d:latest"
+  fi
+}
+
+DIRS="openjdk-7 openjdk-8"
+for d in $DIRS; do
+  build_and_push "$d"
+done
+
+echo "Built all images:"
+: > report.txt
+for image in "${CREATED[@]}"; do
+  docker image list --format "{{.ID}} {{.Repository}}:{{.Tag}}" "$image" >> report.txt
+done
+
+cat >> report.txt <<EOM
+
+To use:
+
+    docker login --username lucidworkstest --password XXX
+    docker pull lucidworks/fusion-builder-openjdk-8:$NOW
+EOM
+
