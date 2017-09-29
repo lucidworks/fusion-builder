5465251843d327ac22816cc7da494941abcc3953
Martijn Koster <mak-github@greenhills.co.uk>
2017-09-29 12:05:15 +0100

    unpin openjdk-8 latest git and Maven add /build.info for debugging add yaml-dev for PyYAML remove openssh-server and EXPOSE add gitignore add jenkins.sh for building

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-sed -e 's,FROM_GOES_HERE,java:openjdk-7,' -e 's, openjfx,,' \
-  Dockerfile.template > openjdk-7/Dockerfile
-sed -e 's,FROM_GOES_HERE,java:openjdk-8u66,' \
-  Dockerfile.template > openjdk-8/Dockerfile
-sed -e 's,FROM_GOES_HERE,makuk66/docker-oracle-java7,' -e 's, openjfx,,' \
-  Dockerfile.template > oracle-jdk-7/Dockerfile
+sed -e 's,FROM_GOES_HERE,java:openjdk-7,' Dockerfile.template > openjdk-7/Dockerfile
+sed -e 's,FROM_GOES_HERE,java:openjdk-8,' Dockerfile.template > openjdk-8/Dockerfile
+sed -e 's,FROM_GOES_HERE,makuk66/docker-oracle-java7,' Dockerfile.template > oracle-jdk-7/Dockerfile
 for d in openjdk-7 openjdk-8 oracle-jdk-7; do
   cp requirements.txt.template $d/requirements.txt
 done
