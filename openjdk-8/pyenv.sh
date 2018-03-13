a8400eda7514c0f589a98ebb59ad0a5a7e9c6d39
Martijn Koster <mak-github@greenhills.co.uk>
2018-03-13 20:48:04 +0000

    update pyenv

--- openjdk-8/pyenv.sh
+++ openjdk-8/pyenv.sh
@@ -5,15 +5,18 @@
 PYTHON_VERSION=2.7.14
 
 cd /home/jenkins
-git clone git://github.com/yyuu/pyenv.git .pyenv
-(cd .pyenv && git checkout v20150601)
-git clone https://github.com/yyuu/pyenv-virtualenv.git .pyenv/plugins/pyenv-virtualenv
+git clone git://github.com/pyenv/pyenv.git .pyenv
+(cd .pyenv && git checkout v1.2.2)
+git clone https://github.com/pyenv/pyenv-virtualenv.git .pyenv/plugins/pyenv-virtualenv
 (cd .pyenv/plugins/pyenv-virtualenv && git checkout v20150526)
 export PYENV_ROOT=$(pwd)/.pyenv
 export PATH=$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH
 cat >>.bashrc <<EOM
 export PYENV_ROOT=\$HOME/.pyenv
 export PATH=\$PYENV_ROOT/shims:\$PYENV_ROOT/bin:\$PATH
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
 EOM
 pyenv install $PYTHON_VERSION
 pyenv global $PYTHON_VERSION
